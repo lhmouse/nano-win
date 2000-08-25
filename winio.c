dabc3cb0df527193c1981322b25fcdd086570fe6
Chris Allegretta <chrisa@asty.org>
2000-08-25 01:44:12 +0000

    Off by one error fix (Rocco Corsi)
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@187 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- winio.c
+++ winio.c
@@ -462,7 +462,7 @@ void onekey(char *keystroke, char *desc)
 {
     char description[80];
 
-    snprintf(description, 12, " %-11s", desc);
+    snprintf(description, 13, " %-11s", desc);
     wattron(bottomwin, A_REVERSE);
     waddstr(bottomwin, keystroke);
     wattroff(bottomwin, A_REVERSE);
