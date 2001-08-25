9b3c7e8be76e7c056d97900f31e996b9f5b9cfe5
Chris Allegretta <chrisa@asty.org>
2001-08-25 16:00:53 +0000

    winio.c: - Add David Lawrence Ramsey to credits
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@749 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- winio.c
+++ winio.c
@@ -1653,7 +1653,7 @@ void do_replace_highlight(int highlight_flag, char *word)
 }
 
 #ifdef NANO_EXTRA
-#define CREDIT_LEN 47
+#define CREDIT_LEN 48
 void do_credits(void)
 {
     int i, j = 0, k, place = 0, start_x;
@@ -1692,6 +1692,7 @@ void do_credits(void)
 			"Joshua Jensen",
 			"Ryan Krebs",
 			"Albert Chin",
+			"David Lawrence Ramsey",
 			"",
 			specialthx,
 			"Plattsburgh State University",
