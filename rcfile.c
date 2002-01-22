34f8098045a075db959d98f42f881ecfed44dd50
Chris Allegretta <chrisa@asty.org>
2002-01-22 20:09:20 +0000

    - rcfile.c:General - Added stdarg.h to includes
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@1030 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- rcfile.c
+++ rcfile.c
@@ -20,6 +20,7 @@
  **************************************************************************/
 
 #include <stdlib.h>
+#include <stdarg.h>
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
