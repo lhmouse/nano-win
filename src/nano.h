a619ae69bf7232647c72e13966bc4aca66a0de87
David Lawrence Ramsey <pooka109@gmail.com>
2004-03-04 06:33:52 +0000

    Tom Bates' nsr-tandem-nsk port, plus a few tweaks of mine, take 2 (according to comments, including floss.h in nano.h does work)
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@1681 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- src/nano.h
+++ src/nano.h
@@ -22,6 +22,14 @@
 #ifndef NANO_H
 #define NANO_H 1
 
+#ifdef __TANDEM
+/* Tandem NonStop Kernel */
+#include <floss.h>
+#define NANO_ROOT_UID 65535
+#else
+#define NANO_ROOT_UID 0
+#endif
+
 #ifdef HAVE_LIMITS_H
 #include <limits.h>
 #endif
