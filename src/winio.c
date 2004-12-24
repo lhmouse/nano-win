7ddca3a9a66b2a13694a542cdcbedef23477286f
David Lawrence Ramsey <pooka109@gmail.com>
2004-12-24 15:23:10 +0000

    fix if block
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@2197 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- src/winio.c
+++ src/winio.c
@@ -1856,7 +1856,7 @@ char *display_string(const char *buf, size_t start_col, size_t len, bool
 		);
 
 	if (is_cntrl_char(wide_buf)) {
-	    if (column > start_col) {
+	    if (dollars && column > start_col) {
 		char *ctrl_wide_buf = charalloc(MB_CUR_MAX);
 		int ctrl_wide_buf_len, i;
 
