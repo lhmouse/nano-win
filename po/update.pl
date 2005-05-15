6e925cf330f290aa72c437672d73944096935adb
David Lawrence Ramsey <pooka109@gmail.com>
2005-05-15 19:57:17 +0000

    update the Free Software Foundation's mailing address in various comments
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@2510 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- po/update.pl
+++ po/update.pl
@@ -5,19 +5,20 @@
 #
 #  Copyright (C) 2000 Free Software Foundation.
 #
-#  This script is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License as
-#  published by the Free Software Foundation; either version 2 of the
-#  License, or (at your option) any later version.
+#  This script is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or (at
+#  your option) any later version.
 #
-#  This script is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  This script is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 #  General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this library; if not, write to the Free Software
-#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301,
+#  USA.
 #
 #  Authors: Kenneth Christiansen <kenneth@gnu.org>
 #
