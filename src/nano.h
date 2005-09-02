093d500e150ae1d516728634cf06b25e957a3a4a
David Lawrence Ramsey <pooka109@gmail.com>
2005-09-02 19:09:36 +0000

    if we don't have regex.h and hence regex support, disable color support, as it depends on the use of regexes
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@3011 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- src/nano.h
+++ src/nano.h
@@ -135,6 +135,12 @@
 
 #define VERMSG "GNU nano " VERSION
 
+/* If we don't have regex support, turn the color support off, as it
+ * depends on the use of regexes. */
+#ifndef HAVE_REGEX_H
+#undef ENABLE_COLOR 
+#endif
+
 /* If we aren't using ncurses, turn the mouse support off, as it's
  * ncurses-specific. */
 #ifndef NCURSES_MOUSE_VERSION
