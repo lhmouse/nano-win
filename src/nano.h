9ffe78aa5793e37386451693cf3ecc77acb798b4
David Lawrence Ramsey <pooka109@gmail.com>
2006-02-03 18:59:17 +0000

    update copyright years
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@3274 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- src/nano.h
+++ src/nano.h
@@ -3,7 +3,7 @@
  *   nano.h                                                               *
  *                                                                        *
  *   Copyright (C) 1999-2004 Chris Allegretta                             *
- *   Copyright (C) 2005 David Lawrence Ramsey                             *
+ *   Copyright (C) 2005-2006 David Lawrence Ramsey                        *
  *   This program is free software; you can redistribute it and/or modify *
  *   it under the terms of the GNU General Public License as published by *
  *   the Free Software Foundation; either version 2, or (at your option)  *
