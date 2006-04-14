f50bd4be884feeae2c3d91b88da01dd678e17641
David Lawrence Ramsey <pooka109@gmail.com>
2006-04-14 20:15:44 +0000

    clarify some help text and documentation
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@3379 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- src/global.c
+++ src/global.c
@@ -305,7 +305,7 @@ void shortcut_init(bool unjustify)
 #endif
 #ifndef DISABLE_HELP
     const char *nano_cancel_msg = N_("Cancel the current function");
-    const char *nano_help_msg = N_("Invoke the help menu");
+    const char *nano_help_msg = N_("Display the help text");
     const char *nano_exit_msg =
 #ifdef ENABLE_MULTIBUFFER
 	N_("Close the current file buffer/Exit from nano")
@@ -327,7 +327,7 @@ void shortcut_init(bool unjustify)
     const char *nano_uncut_msg =
 	N_("Uncut from the cutbuffer into the current line");
     const char *nano_cursorpos_msg =
-	N_("Show the position of the cursor");
+	N_("Display the position of the cursor");
     const char *nano_spell_msg =
 	N_("Invoke the spell checker, if available");
     const char *nano_gotoline_msg = N_("Go to line and column number");
