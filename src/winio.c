5b17ef85e7567b75216018cb89d912f89bbc94ce
David Lawrence Ramsey <pooka109@gmail.com>
2006-07-31 23:28:27 +0000

    revert erroneous change
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@3830 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- src/winio.c
+++ src/winio.c
@@ -651,12 +651,6 @@ int parse_kbinput(WINDOW *win, bool *meta_key, bool *func_key)
 		retval = ERR;
 		break;
 #endif
-#ifdef KEY_EVENT
-	    /* Slang doesn't support KEY_EVENT. */
-	    case KEY_EVENT:
-		retval = ERR;
-		break;
-#endif
 	}
 
 	/* If our result is an extended keypad value (i.e. a value
