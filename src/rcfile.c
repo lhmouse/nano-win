cb801d63a3825d77dc97a3cae72c87639cc79f84
David Lawrence Ramsey <pooka109@gmail.com>
2006-11-10 02:47:11 +0000

    cosmetic copyright notice fixes
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@3950 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- src/rcfile.c
+++ src/rcfile.c
@@ -2,8 +2,8 @@
 /**************************************************************************
  *   rcfile.c                                                             *
  *                                                                        *
- *   Copyright (C) 2001-2004 Chris Allegretta                             *
- *   Copyright (C) 2005-2006 David Lawrence Ramsey                        *
+ *   Copyright (C) 2001, 2002, 2003, 2004 Chris Allegretta                *
+ *   Copyright (C) 2005, 2006 David Lawrence Ramsey                       *
  *   This program is free software; you can redistribute it and/or modify *
  *   it under the terms of the GNU General Public License as published by *
  *   the Free Software Foundation; either version 2, or (at your option)  *
