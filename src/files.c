b4e5fc100b8144188da763ce332d5f487d1e4926
David Lawrence Ramsey <pooka109@gmail.com>
2007-08-16 03:18:17 +0000

    still more cosmetic fixes
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@4155 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- src/files.c
+++ src/files.c
@@ -876,13 +876,13 @@ void do_insertfile(
 		    edittop_save = openfile->fileage;
 
 		/* Update the current x-coordinate to account for the
-		 * number of characters inserted on the current line. */
+		 * number of characters inserted on the current line.
+		 * If the mark begins inside the partition, adjust the
+		 * mark coordinates to compensate for the change in the
+		 * current line. */
 		openfile->current_x = strlen(openfile->filebot->data);
 		if (openfile->fileage == openfile->filebot) {
 #ifndef NANO_TINY
-		    /* If the mark begins inside the partition, adjust
-		     * the mark coordinates to compensate for the change
-		     * in the current line. */
 		    if (openfile->mark_set) {
 			openfile->mark_begin = openfile->current;
 			if (do_mark_shift)
