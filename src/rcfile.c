2d01e6f779c009b6722b7451e3019fbdca1c9cb1
David Lawrence Ramsey <pooka109@gmail.com>
2007-10-05 16:44:45 +0000

    update copyright notices, as my copyrights have now been assigned to the Free Software Foundation
    git-svn-id: svn://svn.savannah.gnu.org/nano/branches/nano_2_0_branch/nano@4172 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- src/rcfile.c
+++ src/rcfile.c
@@ -3,7 +3,7 @@
  *   rcfile.c                                                             *
  *                                                                        *
  *   Copyright (C) 2001, 2002, 2003, 2004 Chris Allegretta                *
- *   Copyright (C) 2005, 2006, 2007 David Lawrence Ramsey                 *
+ *   Copyright (C) 2005, 2006, 2007 Free Software Foundation, Inc.        *
  *   This program is free software; you can redistribute it and/or modify *
  *   it under the terms of the GNU General Public License as published by *
  *   the Free Software Foundation; either version 3, or (at your option)  *
