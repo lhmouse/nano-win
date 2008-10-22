2d189ae46de5c3b4a0dacbceda7cba78d561faee
Chris Allegretta <chrisa@asty.org>
2008-10-22 22:11:46 +0000

    Add tiny check around new mtime check code. Fixes certain compile time options.
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@4345 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- src/files.c
+++ src/files.c
@@ -2028,12 +2028,14 @@ bool do_writeout(bool exiting)
 			    continue;
 		    }
 		}
+#ifndef NANO_TINY
 		if (openfile->current_stat && openfile->current_stat->st_mtime < st.st_mtime) {
 		    i = do_yesno_prompt(FALSE,
 			_("File was modified since you opened it, continue saving ? "));
 		    if (i == 0 || i == -1)
 			continue;
 		}
+#endif
 
 	    }
 
