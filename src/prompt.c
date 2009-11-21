879dfbaf40b73c097e179db4f31975df20dc9351
Chris Allegretta <chrisa@asty.org>
2009-11-21 17:12:28 +0000

    * prompt.c (update_statusbar_line): Set statusbar_pww when updating the line.           Fixes Savannah bug 24957 by Mike Frysinger.
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@4434 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- src/prompt.c
+++ src/prompt.c
@@ -868,7 +868,7 @@ void update_statusbar_line(const char *curranswer, size_t index)
     free(expanded);
 
     wattroff(bottomwin, reverse_attr);
-
+    statusbar_pww = statusbar_xplustabs();
     reset_statusbar_cursor();
     wnoutrefresh(bottomwin);
 }
