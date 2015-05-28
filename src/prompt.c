2661d6d397f35b2e83f62fb3e404735c3770f31c
Benno Schulenberg <bensberg@justemail.net>
2015-05-28 13:51:03 +0000

    Using 'width' instead of hardcoded 16.  And doing things in the same order for all three cases: setting the string and then positioning the cursor.
    git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@5230 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

--- src/prompt.c
+++ src/prompt.c
@@ -1077,7 +1077,7 @@ int do_yesno_prompt(bool all, const char *msg)
 
 	if (!ISSET(NO_HELP)) {
 	    char shortstr[3];
-		/* Temp string for Yes, No, All. */
+		/* Temporary string for (translated) " Y", " N" and " A". */
 
 	    if (COLS < 32)
 		width = COLS / 2;
@@ -1091,16 +1091,16 @@ int do_yesno_prompt(bool all, const char *msg)
 	    onekey(shortstr, _("Yes"), width);
 
 	    if (all) {
-		wmove(bottomwin, 1, width);
 		shortstr[1] = allstr[0];
+		wmove(bottomwin, 1, width);
 		onekey(shortstr, _("All"), width);
 	    }
 
-	    wmove(bottomwin, 2, 0);
 	    shortstr[1] = nostr[0];
+	    wmove(bottomwin, 2, 0);
 	    onekey(shortstr, _("No"), width);
 
-	    wmove(bottomwin, 2, 16);
+	    wmove(bottomwin, 2, width);
 	    onekey("^C", _("Cancel"), width);
 	}
 
