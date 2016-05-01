lhmouse/nano-win

commit 8704dde8736bf702ff470b39ea8ad3413b3e4486

Preventing the internal spell checker from finding the first occurrence of a misspelled word twice.  And deleting the piece of dead code that was meant to do this.  This fixes Savannah bug #47188.
When we've reached again the same line where we started and we find an
instance there, then this can only be /before or at/ the position from
where we started, otherwise we would have found it when we commenced
searching.  And so... that little piece of dead code does absolutely
nothing -- it will never fire.

It's so nice... nano is full of Easter Eggs!  :)


git-svn-id: svn://svn.savannah.gnu.org/nano/trunk/nano@5760 35c25a1d-7b9e-4130-9fde-d3aeb78583b8

src/search.c
@@ -300,6 +300,13 @@ bool findnextstr(
 #ifndef DISABLE_SPELLER
 	    bool found_whole = FALSE;
 		/* Is this potential match a whole word? */
+
+	    /* When we're spell-checking, don't search in the starting line
+	     * again -- there is no need: we started at x = 0. */
+	    if (whole_word_only && search_last_line) {
+		disable_nodelay();
+		return FALSE;
+	    }
 #endif
 
 	    /* Set found_len to the length of the potential match. */
@@ -377,20 +384,8 @@ bool findnextstr(
     /* We found an instance. */
     current_x_find = found - fileptr->data;
 
-    /* Ensure we haven't wrapped around again! */
-    if (search_last_line &&
-#ifndef NANO_TINY
-		((!ISSET(BACKWARDS_SEARCH) && current_x_find > begin_x) ||
-		(ISSET(BACKWARDS_SEARCH) && current_x_find < begin_x))) {
-#else
-		current_x_find > begin_x) {
-#endif
-	not_found_msg(needle);
-	disable_nodelay();
-	return FALSE;
-    }
-
     disable_nodelay();
+
     /* We've definitely found something. */
     openfile->current = fileptr;
     openfile->current_x = current_x_find;

commit 9f1a44d9e3aaac48ea884b0a4b7901a3d2528432

replacing: properly detect when we've rereached the starting position
Commit 8704dde mistakenly removed this part of code -- it is not dead,
it is just that it will only fire when the user answered No at some of
the replacement prompts.  So... when we've rereached the starting line,
a found occurrence is invalid when it is beyond the starting x (either
after or before it, dependending on the direction of search).

This fixes https://savannah.gnu.org/bugs/?47816.

src/search.c
@@ -266,6 +266,8 @@ int findnextstr(
 	/* When bigger than zero, show and wipe the "Searching..." message. */
     filestruct *fileptr = openfile->current;
     const char *rev_start = fileptr->data, *found = NULL;
+    size_t found_x;
+	/* The x coordinate of a found occurrence. */
     time_t lastkbcheck = time(NULL);
 
     /* rev_start might end up 1 character before the start or after the
@@ -380,11 +382,28 @@ int findnextstr(
 #endif
     }
 
+    found_x = found - fileptr->data;
+
+    /* Ensure that the found occurrence is not beyond the starting x. */
+    if (search_last_line &&
+#ifndef NANO_TINY
+		((!ISSET(BACKWARDS_SEARCH) && found_x > begin_x) ||
+		(ISSET(BACKWARDS_SEARCH) && found_x < begin_x))
+#else
+		found_x > begin_x
+#endif
+		) {
+	not_found_msg(needle);
+	disable_nodelay();
+	return 0;
+    }
+
+
     disable_nodelay();
 
     /* Set the current position to point at what we found. */
     openfile->current = fileptr;
-    openfile->current_x = found - fileptr->data;
+    openfile->current_x = found_x;
     openfile->current_y = fileptr->lineno - openfile->edittop->lineno;
 
     /* When requested, pass back the length of the match. */