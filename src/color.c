406e5242a34b6ebb0a445ed65c47fe47905efe81
Benno Schulenberg <bensberg@justemail.net>
2016-08-29 15:14:18 +0200

    update the copyright notices

--- src/color.c
+++ src/color.c
@@ -3,6 +3,8 @@
  *                                                                        *
  *   Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,  *
  *   2010, 2011, 2013, 2014, 2015 Free Software Foundation, Inc.          *
+ *   Copyright (C) 2014, 2015, 2016 Benno Schulenberg                     *
+ *                                                                        *
  *   This program is free software; you can redistribute it and/or modify *
  *   it under the terms of the GNU General Public License as published by *
  *   the Free Software Foundation; either version 3, or (at your option)  *
