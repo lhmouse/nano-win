ba8d71f4478bff4d76bbc2f5fbda60ec1f7a7739
Mike Frysinger <vapier@gentoo.org>
2017-02-21 17:04:35 -0500

    add support for gnulib
    On a system with up-to-date GNU tools, this does not change
    the size of nano's binary.
    
    We start off with importing only a few modules, although we
    don't yet delete the fallback logic for them.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 # Generate configure & friends for GIT users.
 
+gnulib_url="git://git.sv.gnu.org/gnulib.git"
+gnulib_hash="4084b3a1094372b960ce4a97634e08f4538c8bdd"
+
+modules="
+	strcase
+	strcasestr-simple
+	strnlen
+"
+
+# Make sure the local gnulib git repo is up-to-date.
+if [ ! -d "gnulib" ]; then
+	git clone --depth=123 ${gnulib_url}
+fi
+cd gnulib >/dev/null || exit 1
+curr_hash=$(git log -1 --format=%H)
+if [ "${gnulib_hash}" != "${curr_hash}" ]; then
+	git pull
+	git checkout -f ${gnulib_hash}
+fi
+cd .. >/dev/null || exit 1
+
+rm -rf lib
+./gnulib/gnulib-tool \
+	--import \
+	${modules}
+
 autoreconf -f -i -s
