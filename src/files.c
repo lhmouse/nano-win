261b5b28c4a1c1cba73356a061c048c5fa2be75f
Benno Schulenberg <bensberg@justemail.net>
2017-03-23 22:08:57 +0100

    tweaks: gettextize a forgotten error message

--- src/files.c
+++ src/files.c
@@ -1451,7 +1451,7 @@ void init_operating_dir(void)
 
     /* If the operating directory is inaccessible, fail. */
     if (full_operating_dir == NULL || chdir(full_operating_dir) == -1)
-	die("Invalid operating directory\n");
+	die(_("Invalid operating directory\n"));
 
     snuggly_fit(&full_operating_dir);
 }
