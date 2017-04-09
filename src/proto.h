754c62c5cce6fb933a943f618118ccc25adf4968
Benno Schulenberg <bensberg@justemail.net>
2017-04-09 12:09:23 +0200

    copyright: update the years, use ranges, and explain this usage
    The interval 2013-2017 for the Free Software Foundation is valid
    because in those years there were releases with changes by either
    Chris or David, and the GNU maintainers guide advises to mention
    a new year in all files of a package, not just in the ones that
    actually changed, and be done with it for the rest of the year.

--- src/proto.h
+++ src/proto.h
@@ -1,8 +1,7 @@
 /**************************************************************************
  *   proto.h  --  This file is part of GNU nano.                          *
  *                                                                        *
- *   Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,  *
- *   2008, 2009, 2010, 2011, 2013, 2014 Free Software Foundation, Inc.    *
+ *   Copyright (C) 1999-2011, 2013-2017 Free Software Foundation, Inc.    *
  *                                                                        *
  *   GNU nano is free software: you can redistribute it and/or modify     *
  *   it under the terms of the GNU General Public License as published    *
