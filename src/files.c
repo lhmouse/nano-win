8075e454a9f717dc60eb55c0b0afc24d8be3d81d
Benno Schulenberg <bensberg@justemail.net>
2017-05-17 10:38:36 +0200

    history: don't save the cursor position for temporary help files
    It's useless, because the position doesn't get restored, and it
    cannot be restored because the name is different every time.

--- src/files.c
+++ src/files.c
@@ -2994,7 +2994,7 @@ void update_poshistory(char *filename, ssize_t lineno, ssize_t xpos)
     poshiststruct *posptr, *theone, *posprev = NULL;
     char *fullpath = get_full_path(filename);
 
-    if (fullpath == NULL || fullpath[strlen(fullpath) - 1] == '/') {
+    if (fullpath == NULL || fullpath[strlen(fullpath) - 1] == '/' || inhelp) {
 	free(fullpath);
 	return;
     }
