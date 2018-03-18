9517972b58aa49faacf7b29dbca8da5a317b2da8
Benno Schulenberg <bensberg@telfort.nl>
2018-03-18 16:33:21 +0100

    build: fix compilation again when configured with --enable-tiny

--- src/nano.c
+++ src/nano.c
@@ -1628,7 +1628,9 @@ bool wanted_to_move(void *func)
 			func == do_up || func == do_down ||
 			func == do_home || func == do_end ||
 			func == do_prev_word_void || func == do_next_word_void ||
+#ifndef NANO_TINY
 			func == do_para_begin_void || func == do_para_end_void ||
+#endif
 			func == do_prev_block || func == do_next_block ||
 			func == do_page_up || func == do_page_down ||
 			func == to_first_line || func == to_last_line;
