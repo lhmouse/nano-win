lhmouse/nano-win

commit 54103d8ed10972fb57bce8fb559ccb97ac1ac649

tweaks: elide another variable, to call a function less often
Instead of always calling sctofunc(), it is now only called when
in view mode OR when (after the keystroke's function has been run)
no need for a refresh has been established yet.

src/nano.c
@@ -1708,9 +1708,7 @@ int do_input(bool allow_funcs)
 	if (shortcut == NULL)
 		pletion_line = NULL;
 	else {
-		const subnfunc *f = sctofunc(shortcut);
-
-		if (ISSET(VIEW_MODE) && f && !f->viewok) {
+		if (ISSET(VIEW_MODE) && !sctofunc(shortcut)->viewok) {
 			print_view_warning();
 			return ERR;
 		}
@@ -1776,7 +1774,7 @@ int do_input(bool allow_funcs)
 				wrap_reset();
 #endif
 #ifdef ENABLE_COLOR
-			if (f && !f->viewok && !refresh_needed)
+			if (!refresh_needed && !sctofunc(shortcut)->viewok)
 				check_the_multis(openfile->current);
 #endif
 			if (!refresh_needed && (shortcut->func == do_delete ||

commit b830a7dd384495fb9b882f866b26948025abe136

input: do not crash if sctofunc() returns NULL
This fixes a regression introduced by commit 54103d8e: a crash that
can be triggered by running 'nano --restrict' and pressing <Insert>.

This addresses https://bugzilla.redhat.com/1558532.

src/nano.c
@@ -1727,9 +1727,12 @@ int do_input(bool allow_funcs)
 	if (shortcut == NULL)
 		pletion_line = NULL;
 	else {
-		if (ISSET(VIEW_MODE) && !sctofunc(shortcut)->viewok) {
-			print_view_warning();
-			return ERR;
+		if (ISSET(VIEW_MODE)) {
+			const subnfunc *f = sctofunc(shortcut);
+			if (f && !f->viewok) {
+				print_view_warning();
+				return ERR;
+			}
 		}
 
 		/* If the function associated with this shortcut is
@@ -1801,8 +1804,11 @@ int do_input(bool allow_funcs)
 				wrap_reset();
 #endif
 #ifdef ENABLE_COLOR
-			if (!refresh_needed && !sctofunc(shortcut)->viewok)
-				check_the_multis(openfile->current);
+			if (!refresh_needed) {
+				const subnfunc *f = sctofunc(shortcut);
+				if (f && !f->viewok)
+					check_the_multis(openfile->current);
+			}
 #endif
 			if (!refresh_needed && (shortcut->func == do_delete ||
 									shortcut->func == do_backspace))