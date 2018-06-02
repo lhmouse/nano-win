33d81fa1e62ddc1a45e13f53f5ed7e84f42e7d44
Benno Schulenberg <bensberg@telfort.nl>
2018-06-02 11:18:04 +0200

    build: add the release script to the repository

---
+++ roll-a-release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VERSION="2.9.8"
+
+echo "Pulling..." &&  git pull &&
+echo "Running autogen..." &&  ./autogen.sh &&
+rm -v -f m4/*.m4~ *.asc *.sig *.gz *.xz &&
+
+echo "Rebuilding..." &&  make &&
+po/update_linguas.sh &&
+
+make distcheck &&  make dist-xz &&
+
+git commit -a -m "$(git log -1 --grep 'po: up' | grep o: | sed 's/^    //')" &&
+
+gpg -a -b nano-$VERSION.tar.gz &&
+gpg -a -b nano-$VERSION.tar.xz &&
+gpg --verify nano-$VERSION.tar.gz.asc &&
+gpg --verify nano-$VERSION.tar.xz.asc &&
+
+git tag -u A0ACE884 -a v$VERSION -m "the nano $VERSION release" &&
+
+for file in nano-$VERSION.tar.*z*; do scp $file bens@wh0rd.org:$file; done &&
+
+gnupload --to ftp.gnu.org:nano  nano-$VERSION.tar.*z &&
+
+echo "Tarballs have been rolled and uploaded."
