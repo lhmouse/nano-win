a4e3d88c67024b5db3019c75e3c9d827128aa7dc
Benno Schulenberg <bensberg@telfort.nl>
2020-10-02 09:18:43 +0200

    version: remove URL and email address from the --version output
    Web links and email addresses do not belong in the version information.
    This has bothered me for a long time.  The URL is in the man page, and
    bug reports should go to Savannah.

--- src/nano.c
+++ src/nano.c
@@ -663,7 +663,6 @@ void version(void)
 #ifndef NANO_TINY
 	printf(" (C) 1999-2011, 2013-2020 Free Software Foundation, Inc.\n");
 	printf(_(" (C) 2014-%s the contributors to nano\n"), "2020");
-	printf(_(" Email: nano@nano-editor.org	Web: https://nano-editor.org/\n"));
 #endif
 	printf(_(" Compiled options:"));
 
