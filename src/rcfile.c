ac5e5179fb30478efb7e40c6cb901d47c8328049
Benno Schulenberg <bensberg@telfort.nl>
2021-03-03 09:31:46 +0100

    options: rename 'highlightcolor' to the more distinct 'spotlightcolor'
    This way there cannot be any confusion with syntax highlighting,
    and it indicates better that a single place will be highlighted.

--- src/rcfile.c
+++ src/rcfile.c
@@ -132,7 +132,7 @@ static const rcoption rcopts[] = {
 	{"stripecolor", 0},
 	{"scrollercolor", 0},
 	{"selectedcolor", 0},
-	{"highlightcolor", 0},
+	{"spotlightcolor", 0},
 	{"promptcolor", 0},
 	{"statuscolor", 0},
 	{"errorcolor", 0},
@@ -1558,7 +1558,7 @@ void parse_rcfile(FILE *rcstream, bool just_syntax, bool intros_only)
 			color_combo[SCROLL_BAR] = parse_interface_color(argument);
 		else if (strcmp(option, "selectedcolor") == 0)
 			color_combo[SELECTED_TEXT] = parse_interface_color(argument);
-		else if (strcmp(option, "highlightcolor") == 0)
+		else if (strcmp(option, "spotlightcolor") == 0)
 			color_combo[HIGHLIGHTED] = parse_interface_color(argument);
 		else if (strcmp(option, "promptcolor") == 0)
 			color_combo[PROMPT_BAR] = parse_interface_color(argument);
