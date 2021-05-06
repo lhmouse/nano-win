4712d4646238cbd012adb1486fe151214600184a
Benno Schulenberg <bensberg@telfort.nl>
2021-05-06 13:06:22 +0200

    linter: block the resizing signal while reading output from the linter
    This prevents read() from returning unexpectedly and causing a crash.
    
    This fixes https://savannah.gnu.org/bugs/?60537.
    Reported-by: Filips Romāns <frfilips@gmail.com>
    
    Bug existed since version 2.4.2, since the handling of SIGWINCH changed.

--- src/text.c
+++ src/text.c
@@ -2610,6 +2610,9 @@ void do_linter(void)
 		return;
 	}
 
+	/* Block resizing signals while reading from the pipe. */
+	block_sigwinch(TRUE);
+
 	/* Read in the returned syntax errors. */
 	totalread = 0;
 	buffersize = pipesize + 1;
@@ -2625,6 +2628,8 @@ void do_linter(void)
 
 	*pointer = '\0';
 	close(lint_fd[0]);
+
+	block_sigwinch(FALSE);
 
 	/* Process the linter output. */
 	pointer = lintings;
