50106266bc6fa83c2d141e3f7b44d62df2adc84c
Benno Schulenberg <bensberg@telfort.nl>
2021-10-21 12:27:22 +0200

    pasting: when less than a line is pasted, allow automatic hard-wrapping
    When --breaklonglines is in effect and the user pasted just a few words
    (anything without a linebreak), then act as if this short text had been
    typed by the user and hard-wrap the line when it became overlong.
    
    This fulfills https://savannah.gnu.org/bugs/?61353.

--- src/cut.c
+++ src/cut.c
@@ -728,6 +728,10 @@ void paste_text(void)
 	update_undo(PASTE);
 #endif
 
+	/* When still on the same line and doing hard-wrapping, limit the width. */
+	if (openfile->current == was_current && ISSET(BREAK_LONG_LINES))
+		do_wrap();
+
 	/* If we pasted less than a screenful, don't center the cursor. */
 	if (less_than_a_screenful(was_lineno, was_leftedge))
 		focusing = FALSE;
