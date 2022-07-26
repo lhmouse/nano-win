accecb986382ef986758ca369f58edd5b5c13449
Benno Schulenberg <bensberg@telfort.nl>
2022-07-26 08:36:05 +0200

    tweaks: reshuffle a few lines, to group things better

--- src/global.c
+++ src/global.c
@@ -1193,21 +1193,20 @@ void shortcut_init(void)
 	add_to_sclist(MMOST, "^K", 0, cut_text, 0);
 #ifdef NANO_TINY
 	add_to_sclist(MMAIN, "^U", 0, paste_text, 0);
+#ifdef ENABLE_SPELLER
+	add_to_sclist(MMAIN, "^T", 0, do_spell, 0);
+#endif
 #else
 	add_to_sclist(MMOST, "^U", 0, paste_text, 0);
 	add_to_sclist(MMAIN, "^T", 0, do_execute, 0);
-#endif
-#ifdef ENABLE_JUSTIFY
-	add_to_sclist(MMAIN, "^J", '\n', do_justify, 0);
-#endif
 #ifdef ENABLE_SPELLER
-#ifndef NANO_TINY
 	if (!ISSET(PRESERVE))
 		add_to_sclist(MEXECUTE, "^S", 0, do_spell, 0);
 	add_to_sclist(MEXECUTE, "^T", 0, do_spell, 0);
-#else
-	add_to_sclist(MMAIN, "^T", 0, do_spell, 0);
 #endif
+#endif
+#ifdef ENABLE_JUSTIFY
+	add_to_sclist(MMAIN, "^J", '\n', do_justify, 0);
 #endif
 #ifdef ENABLE_COLOR
 	add_to_sclist(MMAIN, "M-B", 0, do_linter, 0);
