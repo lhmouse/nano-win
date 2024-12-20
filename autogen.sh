94ed5483663221d113b1c52e499a0504cbc191c6
Benno Schulenberg <bensberg@telfort.nl>
2024-12-20 12:15:44 +0100

    gnulib: update to its current upstream state

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 # Generate configure & friends for GIT users.
 
 gnulib_url="git://git.sv.gnu.org/gnulib.git"
-gnulib_hash="6db3edf95439f7087e60fda5e79e2c12507a6ad4"
+gnulib_hash="4631e9b4d5d3bd5ba42864c7001cf3509a00c7b3"
 
 modules="
 	canonicalize-lgpl
