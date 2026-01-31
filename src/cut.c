lhmouse/nano-win

commit 7779a1990312eeea570365e91dce2c5571da5955

new default feature: smooth whole-window sideways scrolling
Instead of horizontally scrolling just the current line by nearly
a screen width when the cursor is about to go offscreen, now all
screen lines are scrolled horizontally together by just the amount
needed to keep the cursor comfortably in view.

References:
  https://lists.gnu.org/archive/html/nano-devel/2026-01/msg00000.html
  https://lists.gnu.org/archive/html/nano-devel/2026-01/msg00003.html
  https://lists.gnu.org/archive/html/nano-devel/2014-05/msg00025.html
  https://lists.gnu.org/archive/html/nano-devel/2014-06/msg00152.html

Nano could have acquired this feature nearly twelve years ago, but...
somehow that old patch fell through the cracks and was forgotten.  :|

Motivated-by: Xylia Allegretta <xylia.allegretta@gmail.com>
Original-patch-by: Mark Majeres <mark@engine12.com>

src/cut.c
@@ -53,6 +53,9 @@ void expunge(undo_type action)
 		/* When softwrapping, a changed number of chunks requires a refresh. */
 		if (ISSET(SOFTWRAP) && extra_chunks_in(openfile->current) != old_amount)
 			refresh_needed = TRUE;
+		/* When panning, and we have come near edge of the viewport... */
+		else if (!ISSET(SOLO_SIDESCROLL) && openfile->placewewant < brink + CUSHION)
+			refresh_needed = TRUE;
 
 		/* Adjust the mark if it is after the cursor on the current line. */
 		if (openfile->mark == openfile->current &&

src/definitions.h
@@ -143,6 +143,9 @@
 /* The default number of columns from end of line where wrapping occurs. */
 #define COLUMNS_FROM_EOL  8
 
+/* The number of columns the cursor should stay away from the edge. */
+#define CUSHION  3
+
 /* The default comment character when a syntax does not specify any. */
 #define GENERAL_COMMENT_CHARACTER  "#"
 
@@ -376,7 +379,8 @@ enum {
 	USE_MAGIC,
 	MINIBAR,
 	ZERO,
-	MODERN_BINDINGS
+	MODERN_BINDINGS,
+	SOLO_SIDESCROLL
 };
 
 /* Structure types. */

src/global.c
@@ -73,6 +73,8 @@ char *title = NULL;
 bool refresh_needed = FALSE;
 		/* Did a command mangle enough of the buffer that we should
 		 * repaint the screen? */
+size_t brink = 0;
+		/* From which column the edit window is drawn (when panning). */
 bool focusing = TRUE;
 		/* Whether an update of the edit window should center the cursor. */
 

src/nano.c
@@ -1551,6 +1551,10 @@ void inject(char *burst, size_t count)
 
 	openfile->placewewant = xplustabs();
 
+	/* When panning, and we have come near the edge of the viewport... */
+	if (!ISSET(SOLO_SIDESCROLL) && openfile->placewewant > brink + editwincols - CUSHION - 1 )
+		refresh_needed = TRUE;
+
 #ifndef NANO_TINY
 	/* When softwrapping and the number of chunks in the current line changed,
 	 * or we were on the last row of the edit window and moved to a new chunk,

src/prototypes.h
@@ -47,6 +47,8 @@ extern int final_status;
 extern bool inhelp;
 extern char *title;
 
+extern size_t brink;
+
 extern bool focusing;
 
 extern bool as_an_at;

src/search.c
@@ -332,6 +332,11 @@ int findnextstr(const char *needle, bool whole_word_only, int modus,
 		spotlighted = TRUE;
 		light_from_col = xplustabs();
 		light_to_col = wideness(line->data, found_x + found_len);
+
+		/* When panning, ensure the end of the match will be visible too. */
+		if (!ISSET(SOLO_SIDESCROLL) && !ISSET(SOFTWRAP))
+			brink = get_page_start(light_to_col);
+
 		refresh_needed = TRUE;
 	}
 #endif
@@ -590,6 +595,9 @@ ssize_t do_replace_loop(const char *needle, bool whole_word_only,
 			light_from_col = xplustabs();
 			light_to_col = wideness(openfile->current->data,
 										openfile->current_x + match_len);
+
+			if (!ISSET(SOLO_SIDESCROLL) && !ISSET(SOFTWRAP))
+				brink = get_page_start(light_to_col);
 
 			/* Refresh the edit window, scrolling it if necessary. */
 			edit_refresh();

src/utils.c
@@ -343,6 +343,17 @@ char *free_and_assign(char *dest, char *src)
  * displayed in the edit window when the cursor is at the given column. */
 size_t get_page_start(size_t column)
 {
+	if (!ISSET(SOLO_SIDESCROLL) && !ISSET(SOFTWRAP)) {
+		if (column < CUSHION)
+			return 0;
+		else if (column < brink + CUSHION)
+			return column - CUSHION;
+		else if (column > brink + editwincols - CUSHION - 1)
+			return column - editwincols + CUSHION + 1;
+		else
+			return brink;
+	}
+
 	if (column == 0 || column + 2 < editwincols || ISSET(SOFTWRAP))
 		return 0;
 	else if (editwincols > 8)

src/winio.c
@@ -2838,7 +2838,10 @@ int update_line(linestruct *line, size_t index)
 #endif
 
 	row = line->lineno - openfile->edittop->lineno;
-	from_col = get_page_start(wideness(line->data, index));
+	if (!ISSET(SOLO_SIDESCROLL))
+		from_col = brink;
+	else
+		from_col = get_page_start(wideness(line->data, index));
 
 	/* Expand the piece to be drawn to its representable form, and draw it. */
 	converted = display_string(line->data, from_col, editwincols, TRUE, FALSE);
@@ -2931,9 +2934,13 @@ bool line_needs_update(const size_t old_column, const size_t new_column)
 #ifndef NANO_TINY
 	if (openfile->mark)
 		return TRUE;
-	else
 #endif
-		return (get_page_start(old_column) != get_page_start(new_column));
+	if (get_page_start(old_column) == get_page_start(new_column))
+		return FALSE;
+	if (!ISSET(SOLO_SIDESCROLL) && !ISSET(SOFTWRAP))
+		refresh_needed = TRUE;
+
+	return !refresh_needed;
 }
 
 /* Try to move up nrows softwrapped chunks from the given line and the
@@ -3342,6 +3349,9 @@ void edit_redraw(linestruct *old_current, update_type manner)
 		adjust_viewport(ISSET(JUMPY_SCROLLING) ? CENTERING : manner);
 		refresh_needed = TRUE;
 		return;
+	} else if (!ISSET(SOLO_SIDESCROLL) && !ISSET(SOFTWRAP)) {
+		refresh_needed = TRUE;
+		return;
 	}
 
 #ifndef NANO_TINY
@@ -3380,6 +3390,10 @@ void edit_refresh(void)
 	/* If the current line is out of view, get it back on screen. */
 	if (current_is_offscreen())
 		adjust_viewport((focusing || ISSET(JUMPY_SCROLLING)) ? CENTERING : FLOWING);
+
+	/* When panning, ensure the cursor will be within the viewport. */
+	if (!ISSET(SOLO_SIDESCROLL) && !ISSET(SOFTWRAP))
+		brink = get_page_start(xplustabs());
 
 #ifdef ENABLE_COLOR
 	/* When needed and useful, initialize the colors for the current syntax. */

commit c5902343bd1507e2e14b50d3e072c820caf8443c

display: store `brink` per buffer, to keep the viewport stable
This fixes https://savannah.gnu.org/bugs/?67993.

Bug existed since whole-window sideways scrolling was introduced
in commit 7779a199 from last week.

src/cut.c
@@ -54,7 +54,7 @@ void expunge(undo_type action)
 		if (ISSET(SOFTWRAP) && extra_chunks_in(openfile->current) != old_amount)
 			refresh_needed = TRUE;
 		/* When panning, and we have come near edge of the viewport... */
-		else if (united_sidescroll && openfile->placewewant < brink + CUSHION)
+		else if (united_sidescroll && openfile->placewewant < openfile->brink + CUSHION)
 			refresh_needed = TRUE;
 
 		/* Adjust the mark if it is after the cursor on the current line. */

src/definitions.h
@@ -566,6 +566,8 @@ typedef struct openfilestruct {
 		/* The file's x-coordinate position. */
 	size_t placewewant;
 		/* The preferred column for the cursor. */
+	size_t brink;
+		/* From which column the edit window is drawn (when panning). */
 	ssize_t cursor_row;
 		/* The row in the edit window that the cursor is on. */
 	struct stat *statinfo;

src/files.c
@@ -73,6 +73,7 @@ void make_new_buffer(void)
 	openfile->current = openfile->filetop;
 	openfile->current_x = 0;
 	openfile->placewewant = 0;
+	openfile->brink = 0;
 	openfile->cursor_row = 0;
 
 	openfile->edittop = openfile->filetop;

src/global.c
@@ -75,8 +75,6 @@ bool refresh_needed = FALSE;
 		 * repaint the screen? */
 bool united_sidescroll = TRUE;
 		/* Whether to scroll all lines sideways.  That is: whether to pan. */
-size_t brink = 0;
-		/* From which column the edit window is drawn (when panning). */
 bool focusing = TRUE;
 		/* Whether an update of the edit window should center the cursor. */
 

src/nano.c
@@ -1553,7 +1553,7 @@ void inject(char *burst, size_t count)
 	openfile->placewewant = xplustabs();
 
 	/* When panning, and we have come near the edge of the viewport... */
-	if (united_sidescroll && openfile->placewewant > brink + editwincols - CUSHION - 1 )
+	if (united_sidescroll && openfile->placewewant > openfile->brink + editwincols - CUSHION - 1 )
 		refresh_needed = TRUE;
 
 #ifndef NANO_TINY

src/prototypes.h
@@ -48,7 +48,6 @@ extern bool inhelp;
 extern char *title;
 
 extern bool united_sidescroll;
-extern size_t brink;
 
 extern bool focusing;
 

src/search.c
@@ -335,7 +335,7 @@ int findnextstr(const char *needle, bool whole_word_only, int modus,
 
 		/* When panning, ensure the end of the match will be visible too. */
 		if (united_sidescroll)
-			brink = get_page_start(light_to_col);
+			openfile->brink = get_page_start(light_to_col);
 
 		refresh_needed = TRUE;
 	}
@@ -597,7 +597,7 @@ ssize_t do_replace_loop(const char *needle, bool whole_word_only,
 										openfile->current_x + match_len);
 
 			if (united_sidescroll)
-				brink = get_page_start(light_to_col);
+				openfile->brink = get_page_start(light_to_col);
 
 			/* Refresh the edit window, scrolling it if necessary. */
 			edit_refresh();

src/utils.c
@@ -346,12 +346,12 @@ size_t get_page_start(size_t column)
 	if (united_sidescroll) {
 		if (column < CUSHION)
 			return 0;
-		else if (column < brink + CUSHION)
+		else if (column < openfile->brink + CUSHION)
 			return column - CUSHION;
-		else if (column > brink + editwincols - CUSHION - 1)
+		else if (column > openfile->brink + editwincols - CUSHION - 1)
 			return column - editwincols + CUSHION + 1;
 		else
-			return brink;
+			return openfile->brink;
 	}
 
 	if (column == 0 || column + 2 < editwincols || ISSET(SOFTWRAP))

src/winio.c
@@ -2839,7 +2839,7 @@ int update_line(linestruct *line, size_t index)
 
 	row = line->lineno - openfile->edittop->lineno;
 	if (united_sidescroll)
-		from_col = brink;
+		from_col = openfile->brink;
 	else
 		from_col = get_page_start(wideness(line->data, index));
 
@@ -3393,7 +3393,7 @@ void edit_refresh(void)
 
 	/* When panning, ensure the cursor will be within the viewport. */
 	if (united_sidescroll)
-		brink = get_page_start(xplustabs());
+		openfile->brink = get_page_start(xplustabs());
 
 #ifdef ENABLE_COLOR
 	/* When needed and useful, initialize the colors for the current syntax. */